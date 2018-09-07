aaf8f7e705ba10c7464a650ed1829a8d75fdf71f
John Tucker <john.tucker@sharpspring.com>
2018-09-07 05:22:44 -0400

    Worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,15 +1,20 @@
 import kue from 'kue';
 
 const { REDIS_URL } = process.env;
-const queue = kue.createQueue({
-  redis: REDIS_URL,
-});
-queue.process('mytype', (job, done) => {
-  switch (job.data.letter) {
-    case 'a':
-      done(null, 'apple');
-      break;
-    default:
-      done(null, 'unknown');
-  }
-});
+
+try {
+  const queue = kue.createQueue({
+    redis: REDIS_URL,
+  });
+  queue.process('mytype', (job, done) => {
+    switch (job.data.letter) {
+      case 'a':
+        done(null, 'apple');
+        break;
+      default:
+        done(null, 'unknown');
+    }
+  });
+} catch (error) {
+  console.log(error);
+}
